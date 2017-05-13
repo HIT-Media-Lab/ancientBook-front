90b555e596d49dad0df6eade48ca06c5041a8158
Alanjoker129 <jiang9071@qq.com>
2017-05-13 21:37:34 +0800

    验证码刷新

--- src/login.vue
+++ src/login.vue
@@ -19,7 +19,7 @@
                 <input type="text"  class="verification_code_input" v-model="v">
                 <input type="button" value="验证码" class="verification_code_button" @click="GetCode()">
                 <!--验证码获取位置-->
-                <img src="/ancient_books/get_v_picture.action" class="code-img" alt="验证码">
+                <img src="/ancient_books/get_v_picture.action" class="code-img" onclick="this.src=this.src+'?'+(new Date()).getTime()" alt="验证码">
 
             </div>
             <button class="login_button" @click="Login()" v-bind:disabled="disabled">登  录</button>
