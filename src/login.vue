3f7a02692b34e68e0f7c2c8658ea3e78745c0a97
Alanjoker129 <jiang9071@qq.com>
2017-05-22 20:41:56 +0800

    修改权限控制

--- src/login.vue
+++ src/login.vue
@@ -158,12 +158,12 @@ import store from './store'
              }
              if (response.body.result == 1) {
                  if (response.body.su == 1) {
-                     this.$store.commit('CHANGE_IFLOGIN1');
+                     this.$store.commit('change_iflogin1');
                      this.$router.push({path: '/super_user'});
                      console.log("登录成功后的全局Token"+this.$store.getters.GetToken)
                  }
                  if (response.body.su == 0) {
-                     this.$store.commit('CHANGE_IFLOGIN2');
+                     this.$store.commit('change_iflogin2');
                      this.$router.push({path: '/user'});
                  }
              }
@@ -193,14 +193,14 @@ import store from './store'
              this.$http.get(this. autologin_url).then(function (response) {
                  if (response.body.result==1) {
                      if (response.body.su == 1)
-                         this.$store.commit('CHANGE_IFLOGIN2');
+                         this.$store.commit('change_iflogin2');
                          this.$router.push({path: '/user'});
                      if (response.body.su = 0)
-                         this.$store.commit('CHANGE_IFLOGIN1');
+                         this.$store.commit('change_iflogin1');
                          this.$router.push({path: '/super_user'});
                  }
                  if (response.body.result==0)
-                     this.$store.commit('CHANGE_IFLOGIN0');
+                     this.$store.commit('change_iflogin0');
                      this.$router.push({path: '/login'});
              },function () {
                  alert("error")
