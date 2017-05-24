95f26549605deb60190955b1aeb7dee22969791e
Alanjoker129 <jiang9071@qq.com>
2017-05-24 21:52:07 +0800

    header

--- src/main.js
+++ src/main.js
@@ -43,9 +43,8 @@ Vue.prototype.HttpPostForm=function (url,object,success,fail) {
     object.token=this.$store.getters.GetToken;
     console.log("你猜猜token有没有 "+object.token);
     this.$http.post(url, object,
-        {emulateJSON: true,
-         headers: {
-             'Content-Type': 'charset=UTF-8'
+        {headers: {
+             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
          }}   //将json形式转换为form-data
         ).then(function (response) {
         this.response=response;
