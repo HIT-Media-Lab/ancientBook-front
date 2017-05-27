97ce8cee94f3b43979ec6e441858e0352cab838c
Alanjoker129 <jiang9071@qq.com>
2017-05-27 20:39:43 +0800

    测试新的封装函数

--- src/main.js
+++ src/main.js
@@ -16,88 +16,20 @@ Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(Vuex);
 
+//回调success前的函数
+function BeforeSuccess() {
 
-//定义的全局变量
-Vue.prototype.response={};
+}
 
-
-
-//定义的post的vue-router全局函数，以json形式传递数据
-Vue.prototype.HttpJson = function (url, type, params, success, fail) {
-    // this.BeforeHttp(params);
-    params.token=this.$store.getters.GetToken;
-    console.log("你猜猜token有没有 "+params.token);
-    if (type.toLocaleLowerCase() == "get") {
-        this.$http.get(url).then(function (response) {
-            this.response=response;
-            this.BeforeSuccess(this.response);
-            success(this.response);
-        },function () {
-            fail()
-        })
-    } else if (type.toLocaleLowerCase() == "post") {
-        //验证是否有无token
-        this.BeforeHttp(params);
-        params.token = this.$store.getters.GetToken();
-
-        this.$http.post(url, params,
-            {headers:{'Content-Type':'application/json;charset=UTF-8'}}
-        ).then(function (response) {
-            this.response=response;
-            this.BeforeSuccess(this.response);
-            success(this.response);
-            this.AfterSuccess(this.response);
-            // console.log(JSON.stringify(response.body))
-        },function () {
-            fail()
-        })
-    }
-};
-
-
-//定义的post的vue-router全局函数，以form-data形式传递数据
-Vue.prototype.HttpPostForm=function (url,params,success,fail) {
-    // this.BeforeHttp(params);
-    params.token=this.$store.getters.GetToken;
-    console.log("你猜猜token有没有 "+params.token);
-    this.$http.post(url, params,
-        {emulateJSON: true}   //将json形式转换为form-data
-        ).then(function (response) {
-        this.response=response;
-        this.BeforeSuccess(this.response);
-        success(this.response);
-        this.AfterSuccess( this.response);
-    },function () {
-        fail()
-    })
-};
-
-//定义的get的vue-router全局函数，以json形式传递数据
-// Vue.prototype.HttpGetJson=function (url,params,success,fail) {
-//     //get的数据传递格式
-//     this.$http.get(url).then(function (response) {
-//         this.response=response;
-//         this.BeforeSuccess();
-//         success(this.response);
-//     },function () {
-//         fail()
-//     })
-// };
-//定义的get的vue-router全局函数，以form-data形式传递数据
-// Vue.prototype.HttpGetForm=function (url,object,success,fail) {
-//     let new_url=url+'?'+object.key+'='+object.value;
-//     this.$http.get(new_url,
-//         {emulateJSON: true}).then(function (response) {
-//         this.response=response;
-//         this.BeforeSuccess();
-//         success(this.response);
-//     },function () {
-//         fail()
-//     })
-// };
+//回调success后的函数
+function AfterSuccess (response) {
+//更新token
+    this.$store.commit("change_token",response.body.token);
+    console.log("更新token"+this.$store.getters.GetToken);
+}
 
 //判断是否有无token
-Vue.prototype.BeforeHttp=function (object) {
+function BeforeHttp (object) {
     console.log("检查超级用户是否有token "+this.$store.getters.GetToken);
     object.token=this.$store.getters.GetToken;
     console.log("object"+object.token);
@@ -111,26 +43,76 @@ Vue.prototype.BeforeHttp=function (object) {
     }else {
         console.log("不需要更token");
     }
+}
+
+
+
+
+//定义的post的vue-router全局函数，以json形式传递数据
+Vue.prototype.HttpJson = function (url, type, params, success, fail) {
+    // this.BeforeHttp(params);
+
+    params.token=this.$store.getters.GetToken;
+    console.log("你猜猜token有没有 "+params.token);
+    if (type.toLocaleLowerCase() == "get") {
+        this.$http.get(url).then(function (response) {
+            response_handle(response, success, fail)
+        },function () {
+            error();
+        })
+    } else if (type.toLocaleLowerCase() == "post") {
+        //验证是否有无token
+        BeforeHttp(params);
+        params.token = this.$store.getters.GetToken();
+
+        this.$http.post(url, params,
+            {headers:{'Content-Type':'application/json;charset=UTF-8'}}
+        ).then(function (response) {
+            response_handle(response);
+        },function () {
+            fail()
+        })
+    }
 };
 
-//回调success前的函数
-Vue.prototype.BeforeSuccess=function () {
+/**
+ * 请求发送失败的函数
+ */
+function error() {
 
+}
+
+function response_handle(response, success, fail) {
+    let status = response.status;
+    if (status == 200){
+        if (response.body.result == 1){
+            BeforeSuccess(response);
+            success(response);
+            AfterSuccess(response);
+        } else if (response.body.result == 0){
+            fail();
+        }
+    } else if (status == 403){
+        this.$router.push('/403');
+    } else if (status == 404){
+        this.$router.push('/404');
+    } else if (status == 500){
+        this.$router.push('/500');
+    }
+}
+
+//定义的post的vue-router全局函数，以form-data形式传递数据
+Vue.prototype.HttpPostForm=function (url,params,success,fail) {
+    params.token=this.$store.getters.GetToken;
+    console.log("你猜猜token有没有 "+params.token);
+    this.$http.post(url, params,
+        {emulateJSON: true}   //将json形式转换为form-data
+        ).then(function (response) {
+            response_handle(response, success, fail);
+    },function () {
+       error();
+    })
 };
-
-//回调success后的函数
-Vue.prototype.AfterSuccess=function (response) {
-//更新token
-        this.$store.commit("change_token",response.body.token);
-        console.log("更新token"+this.$store.getters.GetToken);
-};
-
-// Vue.prototype.CheckToken=function () {
-//     this.$http.get('/ancient_books/getToken.action').then(function (response) {
-//         this.Token = response.body.token;
-//         console.log("检测token成功"+this.Token );
-//     })
-// };
 
 // 定义组件, 也可以像教程之前教的方法从别的文件引入
 import  bookstore from './component/bookstore/index.vue'
