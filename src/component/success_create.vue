d3ba351f578b0139de1c250b85570ba6c09dbd74
ZhangXinwen <478066772@qq.com>
2017-06-14 22:47:01 +0800

    修改冲突！！！

--- src/component/success_create.vue
+++ src/component/success_create.vue
@@ -1,8 +1,5 @@
 <template>
-<<<<<<< HEAD
-    <modal :show_modal="show_info">
-        
-=======
+
     <modal :show_modal="show_info" class="zxw-modal-character">
         <div slot="header" class="zxw-character-header">
         </div>
@@ -10,7 +7,6 @@
 
            <p class="zxw-success-create">创建成功</p>
         </div>
->>>>>>> 64280ff421a1f1c3587733aac6158c10ae043ce2
     </modal>
 </template>
 
@@ -31,12 +27,10 @@
 </script>
 
 <style>
-<<<<<<< HEAD
-=======
+
     .zxw-success-create{
         width: 100px;
         margin:0 10% 0 25%;
     }
->>>>>>> 64280ff421a1f1c3587733aac6158c10ae043ce2
 
 </style>
