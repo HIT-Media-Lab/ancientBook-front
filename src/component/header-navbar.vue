2b6dfa211c4df96898e83c3876d543cf20576064
Alanjoker129 <jiang9071@qq.com>
2017-06-19 19:47:09 +0800

    页面刷新的问题

--- src/component/header-navbar.vue
+++ src/component/header-navbar.vue
@@ -81,10 +81,6 @@
               this.name = name;
           });
         },
-        //组件刷新执行钩子
-        beforeMount: function () {
-            this.onload_token();
-        },
         mounted(){
             this.create_v_picture();
         },
@@ -125,44 +121,6 @@
 //                    alert("error")
 //                })
 //            },
-            // 网页启动得到token
-            onload_token(){
-                this.$http.get('/ancient_books/getToken.action').then(function (response) {
-                    console.log("成功得到token");
-                    console.log(response.body.token);
-                    this.$store.commit("change_token",response.body.token);
-                    console.log(this.$store.getters.GetToken + " 第一次获得token");
-                    if (this.$store.getters.GetToken != null){
-                        this.auto_login();
-                    }
-                },function () {
-//                 alert("error")
-                })
-            },
-
-            //自动登录
-            auto_login() {
-                this.$http.get(this. autologin_url).then(function (response) {
-                    if (response.body.result == 1) {
-                        if (response.body.su == 0){
-                            localStorage.setItem('user',JSON.stringify("user"));
-                            this.$router.push({path: '/user'});
-                        }
-                        if (response.body.su == 1){
-                            console.log("跳到了admin");
-                            localStorage.setItem('user',JSON.stringify("admin"));
-                            this.$router.push({path: '/admin'});
-                        }
-                    }
-                    if (response.body.result == 0){
-                        localStorage.setItem('user',JSON.stringify("guest"));
-                        this.$router.push({path: '/login'});
-                    }
-                },function () {
-//                 alert("error")
-                })
-            },
-
             show_login(){
                 let user_id = JSON.parse(localStorage.getItem('user'));
                 if (user_id == 'guest'){
