91f093d34c5cfa09e87480cd1f165bc573554df3
Alanjoker129 <jiang9071@qq.com>
2017-07-03 22:10:51 +0800

    token

--- src/main.js
+++ src/main.js
@@ -483,7 +483,7 @@ Vue.http.interceptors.push((request, next) => {
 
 router.beforeEach( (to, from, next) => {
     if (router.app.$store.getters.GetToken == ''){
-        this.$http.get('/ancient_books/getToken.action').then(function (response) {
+        router.app.$http.get('/ancient_books/getToken.action').then(function (response) {
             console.log("成功得到token");
             console.log(response.body.token);
             router.app.$store.commit("change_token",response.body.token);
