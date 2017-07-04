3314f07cf4865918e51e9f0cc4f2d1e4b7419e34
Alanjoker129 <jiang9071@qq.com>
2017-07-04 20:29:00 +0800

    古籍

--- src/page/bookstore/recent.vue
+++ src/page/bookstore/recent.vue
@@ -2,7 +2,7 @@
     <div class="j-recent">
         <recent_title class="j-recent-bar" :title="this.title"></recent_title>
         <div v-for="item in recent_book" class="j-picture-name">
-            <img v-bind:id=item.id src="" class="j-picture" alt="最近古籍">
+            <img v-bind:id=item.id src="../../assets/img/古籍封面1.jpg" class="j-picture" alt="最近古籍">
             <p style="color: #0f0f0f; text-align: center">{{item.standardName}}</p>
         </div>
     </div>
@@ -14,9 +14,9 @@
         components:{
             recent_title
         },
-//        created: function () {
-//            this.get_id();
-//        },
+        created: function () {
+            this.get_id();
+        },
         data(){
             return{
                 title: '最近',
@@ -31,30 +31,30 @@
             }
         },
         methods: {
-//            get_id(){
-//                this.http_json(this.id_url, 'get',{}, this.success_id, this.fail_id);
-//                console.log("最近");
-//            },
-//            success_id(response){
-//                for (let i = 0; i < response.body.length; i++) {
-//                    let id_url = '';
-//                    console.log("获得最近古籍没问题");
-//                    this.recent_book[i] = response.body[i];
-//                    this.book_cover = {
-//                        'book': '1',
-//                        'volume': '1',
-//                        'page': '1',
-//                        'ancient_book_id': response.body[i].id
-//                    };
+            get_id(){
+                this.http_json(this.id_url, 'get',{}, this.success_id, this.fail_id);
+                console.log("最近");
+            },
+            success_id(response){
+                for (let i = 0; i < response.body.length; i++) {
+                    let id_url = '';
+                    console.log("获得最近古籍没问题");
+                    this.recent_book[i] = response.body[i];
+                    this.book_cover = {
+                        'book': '1',
+                        'volume': '1',
+                        'page': '1',
+                        'ancient_book_id': response.body[i].id
+                    };
 //                    this.http_json(this.picture_page_url, 'get', this.book_cover, this.success_page, this.fail_id);
 //                    id_url = this.picture_url + '?page_id=' + this.page_id;
 //                    document.getElementById(response.body[i].id).src = id_url;
-//                    console.log("得到最近成功")
-//                }
-//            },
-//            fail_id(){
-//
-//            },
+                    console.log("得到最近成功")
+                }
+            },
+            fail_id(){
+
+            },
 //            success_page(response){
 //                this.page_id = response.id
 //            },
