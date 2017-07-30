bca5758667b8a5f1273a8cb01417ce579c5ca844
RenYi <857331700@qq.com>
2017-07-30 08:57:29 +0800

    r

--- src/page/user/mybook/index.vue
+++ src/page/user/mybook/index.vue
@@ -115,7 +115,7 @@
              */
             go_to_bookinfo(id){
                 this.$route.params.ancient_book_id = id;
-                console.log(id);
+                console.log(this.$route.params.ancient_book_id);
                 this.$router.push({name: 'book_varieties', params: this.$route.params});
             },
             go_to_more_privatebook(){
