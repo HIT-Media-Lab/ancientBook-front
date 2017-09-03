c2e2d7324f9a9d866a41ed49e77dbe29b5fd1408
ZhangXinwen <478066772@qq.com>
2017-09-03 14:25:25 +0800

    新建文献本体责任者行为

--- src/page/noumenon/create/two/literature.vue
+++ src/page/noumenon/create/two/literature.vue
@@ -592,10 +592,10 @@
             },
 
             success_action(response){
-                for (let i = 0; i < response.body.g.length; i++) {
+                for (let i = 0; i < response.body.length; i++) {
                     this.action_arr.push({
-                        item_1_id: response.body.g[i].item_1_id,
-                        chinese_name: response.body.g[i].chinese_name
+                        item_1_id: response.body[i].item_1_id,
+                        chinese_name: response.body[i].chinese_name
                     })
                 }
             },
