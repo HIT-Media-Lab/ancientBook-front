beec69f1ed1a1ae8b734a90e11f50397bcbcc062
RenYi <857331700@qq.com>
2017-09-12 10:22:59 +0800

    r

--- src/page/bookstore/ancientbooks/index.vue
+++ src/page/bookstore/ancientbooks/index.vue
@@ -535,18 +535,18 @@
                 }
                 else{
                     this.book_all_info = response.body;
-                    this.book_info_split = response.body.standard_name.split('[');
+                    this.book_info_split = response.body.standard_name.split('(');
                     if (this.book_info_split.length > 1){
-                        this.book_info1 = '[' + this.book_info_split[1];
-                        this.book_info2 = '[' + this.book_info_split[2];
-                        this.book_info3 = '[' + this.book_info_split[3];
-                        this.book_info4 = '[' + this.book_info_split[4];
+                        this.book_info1 = '(' + this.book_info_split[1];
+                        this.book_info2 = '(' + this.book_info_split[2];
+                        this.book_info3 = '(' + this.book_info_split[3];
+                        this.book_info4 = '(' + this.book_info_split[4];
                     }else {
-                        this.book_info_split = response.body.content[i].standard_name.split('【');
-                        this.book_info1 = '【' + this.book_info_split[1];
-                        this.book_info2 = '【' + this.book_info_split[2];
-                        this.book_info3 = '【' + this.book_info_split[3];
-                        this.book_info4 = '【' + this.book_info_split[4];
+                        this.book_info_split = response.body.content[i].standard_name.split('（');
+                        this.book_info1 = '（' + this.book_info_split[1];
+                        this.book_info2 = '（' + this.book_info_split[2];
+                        this.book_info3 = '（' + this.book_info_split[3];
+                        this.book_info4 = '（' + this.book_info_split[4];
                     }
 
                     var get_obj1 = {};
